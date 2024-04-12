d08c0d0d223104a4bf7854dbcd9f89ecc82d8b71
LeonardoMartins016 <ds.45908023841@gmail.com>
2024-04-12 20:54:03 -0300

    finalizando a tabela de IMC

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,34 @@
 const menu = document.querySelector('#menu-interativo');
-const navLinks = document.querySelector('#navLinks')
+const navLinks = document.querySelector('#navLinks');
+const resultado = document.querySelector('#resultIMC');
+
 
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('menu-ativo');
   navLinks.classList.toggle('nav-ativo')
 });
+
+document.getElementById("calculaIMC").addEventListener("click", function(){
+
+  calculadoraIMC(event); 
+
+});
+
+function calculadoraIMC(event) {
+  var Kilo = document.getElementById("kilo").value;
+  var altura = document.getElementById("Altura").value;
+  var kg = parseFloat(Kilo);
+  var metro = parseFloat(altura);
+
+  var mensagemIMC = document.getElementById("resultIMC");
+
+  if (!isNaN(kg && metro)) {
+    mensagemIMC.innerHTML = "Seu IMC é: " + (kg / (metro*metro));
+  } else {
+    mensagemIMC.innerHTML = "Por favor, digite um número válido.";
+  }
+  
+  event.preventDefault();
+
+} 

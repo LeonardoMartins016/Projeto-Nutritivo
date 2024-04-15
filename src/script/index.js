23ecba8206db45aaab60249131a8f86442b10a4a
LeonardoMartins016 <ds.45908023841@gmail.com>
2024-04-14 23:12:48 -0300

    finalizando exibição de classificação de IMC

--- src/script/index.js
+++ src/script/index.js
@@ -20,15 +20,29 @@ function calculadoraIMC(event) {
   var altura = document.getElementById("Altura").value;
   var kg = parseFloat(Kilo);
   var metro = parseFloat(altura);
-
   var mensagemIMC = document.getElementById("resultIMC");
 
-  if (!isNaN(kg && metro)) {
-    mensagemIMC.innerHTML = "Seu IMC é: " + (kg / (metro*metro));
+  var IMC = (kg / (metro*metro)).toFixed(2);
+
+  if (IMC < 18.5 ) {
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e você está ABAIXO do peso.";
+  }else if(IMC <= 24.9) {
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e seu peso está NORMAL."
+  }else if (IMC <= 29.9){
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e você está com PRÉ-OBESIDADE."
+  }else if (IMC <= 34.9 ){
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e você está com grau de OBESIDADE 1"
+  }else if (IMC <= 39.9 ){
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e você está com grau de OBESIDADE 2"
+  }else if (IMC >= 40 ){
+    mensagemIMC.innerHTML = "Seu IMC é " + IMC + " e você está com grau de OBESIDADE 3"
   } else {
     mensagemIMC.innerHTML = "Por favor, digite um número válido.";
   }
-  
+
+
+
+
   event.preventDefault();
 
 } 
